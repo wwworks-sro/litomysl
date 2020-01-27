39ae76116b5e85bcbaae87ff3d0c045d3a7f6e2b
MatyasHajek <hajek@wwworks.cz>
2020-01-27 14:18:09 +0100

    Titulka

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 
     <body class="index-page">
         <?php  include('layouts/_header.php');  ?>
+        <?php  include('layouts/_facebookSide.php');  ?>
         <div id="title-banner">
             <img src="img-system/title/banner.jpg" alt="Letecký pohled - Litomyšl">
             <h2>moderní historické město</h2>
@@ -22,8 +23,7 @@
                  </div>
         </div>
 
-
-        <section id="title-news-calendar">
+        <section id="novinky-kalendar">
             <div class="wrap-main">
                 <div id="news-header">
                     <ul id="news-tabs">
@@ -42,7 +42,7 @@
                             <img src="img-system/title/news-lorem-ipsum-l.jpg" alt="Lorem ipsum dolor">
                             <div class="news-cover"> </div>
                             <div class="news-text">
-                                <h2>Aktualita 1 dolor sit amet consectetuer adip</h2>
+                                <h4>Aktualita 1 dolor sit amet consectetuer adip</h4>
                                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam libero </p>
                             </div>
                         </a>
@@ -50,7 +50,7 @@
                             <img src="img-system/title/news-lorem-ipsum-m.jpg" alt="Lorem ipsum dolor">
                             <div class="news-cover"> </div>
                             <div class="news-text">
-                                <h2>Aktualita 1 dolor sit amet consectetuer adip</h2>
+                                <h5>Aktualita 1 dolor sit amet consectetuer adip</h5>
                                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam libero </p>
                             </div>
                         </a>
@@ -58,7 +58,7 @@
                             <img src="img-system/title/news-lorem-ipsum-m.jpg" alt="Lorem ipsum dolor">
                             <div class="news-cover"> </div>
                             <div class="news-text">
-                                <h2>Aktualita 1 dolor sit amet consectetuer adip</h2>
+                                <h5>Aktualita 1 dolor sit amet consectetuer adip</h5>
                                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam libero </p>
                             </div>
                         </a>
@@ -67,28 +67,28 @@
                         <a href="" class="news-mini">
                             <img src="img-system/title/news-amet-dolor-s.jpg" alt="Lorem ipsum dolor">
                             <div class="news-text">
-                                <h2>Aktualita 1 dolor sit amet consectetuer adip</h2>
+                                <h6>Aktualita 1 dolor sit amet consectetuer adip</h6>
                                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam libero </p>
                             </div>
                         </a>
                         <a href="" class="news-mini">
                             <img src="img-system/title/news-consectetur-s.jpg" alt="Lorem ipsum dolor">
                             <div class="news-text">
-                                <h2>Aktualita 1 dolor sit amet consectetuer adip</h2>
+                                <h6>Aktualita 1 dolor sit amet consectetuer adip</h6>
                                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam libero </p>
                             </div>
                         </a>
                         <a class="news-mini">
                             <img src="img-system/title/news-dolor-sit-2-s.jpg" alt="Lorem ipsum dolor">
                             <div class="news-text">
-                                <h2>Aktualita 1 dolor sit amet consectetuer adip</h2>
+                                <h6>Aktualita 1 dolor sit amet consectetuer adip</h6>
                                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam libero </p>
                             </div>
                         </a>
                         <a href="" class="news-mini">
                             <img src="img-system/title/news-lore-ipsum-s.jpg" alt="Lorem ipsum dolor">
                             <div class="news-text">
-                                <h2>Aktualita 1 dolor sit amet consectetuer adip</h2>
+                                <h6>Aktualita 1 dolor sit amet consectetuer adip</h6>
                                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam libero </p>
                             </div>
                         </a>
@@ -104,20 +104,192 @@
                 </div>
                 <nav id='news-footer-nav'>
                     <ul class="selected-menu-group">
-                        <li> <a href=""> <img src="img-system/title/dots.png" alt="Městský měsíčník Lilie"> lilie</a> </li>
-                        <li> <a href="">úřad</a> </li>
-                        <li> <a href="">lidé</a> </li>
-                        <li> <a href="">vzdělávání</a> </li>
-                        <li> <a href="">kontakty</a> </li>
+                        <li> <a href=""><img src="img-system/title/newspaper.2.3.1.png" alt="Městský měsíčník Lilie"> lilie</a> </li>
+                        <li> <a href=""><img src="img-system/title/video camera 1.2.3.1.png" alt="video reportáž"> video reportáž</a> </li>
+                        <li> <a href=""><img src="img-system/title/presentation.png" alt="úřední deska"> úřední deska</a> </li>
+                        <li> <a href=""><img src="img-system/title/stamp.5.3.1.png" alt="potřebuji zařídit"> potřebuji zařídit</a> </li>
                     </ul>
                 </nav>
 
             </div>
             <div class="wrap-side">
+                <h2 class="today-events-title">KAM DNES?</h2>
+                <a class="calendar-title-link" href="">kalendář akcí</a>
 
+                <div class="calendar-title">
+                    <a href="">
+                        <img src="img-system/title/event-drama.png" alt="divadlo">
+                        <div>
+                            <h6>Lorem ipsum dolor smet, lo ipsum dolor sit amet</h6>
+                            <p>8.00 / Smetanovo náměstí</p>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img src="img-system/title/event-music.png" alt="koncert">
+                        <div>
+                            <h6>Lorem ipsum dolor smet, lo ipsum dolor sit amet</h6>
+                            <p>8.00 / Smetanovo náměstí</p>
+                        </div>
+                    </a>
+
+                    <div class="separator-long-term-events"><span>PROBÍHAJÍCÍ AKCE</span></div>
+
+                    <a href="">
+                        <img src="img-system/title/event-discussion.png" alt="přednáška">
+                        <div>
+                            <h6>Lorem ipsum dolor smet, lo ipsum dolor sit amet</h6>
+                            <p>8.00 / Smetanovo náměstí</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div calendar-title-links>
+                    <a class="link-tommorrow" href="">kam zítra</a>
+                    <a class="top-events" href="">TOP akce</a>
+                </div>
             </div>
+
+
+            <a class="next-section-link" href="#sluzby">Služby</a>
         </section>
 
+        <section id="sluzby">
+            <div class="wrap-main">
+                <h2>Služby</h2>
+                <a href="">Hlášení závad</a>
+
+                <nav id='service-nav-icons'>
+                    <ul>
+                        <li><a href=""><img src="img-system/title/services/house.png" alt="Bydlení">Bydlení</a></li>
+                        <li><a href=""><img src="img-system/title/services/road straight.3.3.1.png" alt="Doprava">Doprava</a></li>
+                        <li><a href=""><img src="img-system/title/services/briefcase.3.3.1.png" alt="Práce">Práce</a></li>
+                        <li><a href=""><img src="img-system/title/services/wedding.png" alt="Svatby">Svatby</a></li>
+                        <li><a href=""><img src="img-system/title/services/grave.png" alt="Hřbitov">Hřbitov</a></li>
+                        <li><a href=""><img src="img-system/title/services/coins 2.3.3.1.png" alt="Dotace města">Dotace města</a></li>
+                    </ul>
+
+                    <ul class="service-nav-icons-red">
+                        <li><a href=""><img src="img-system/title/services/plus.png" alt="Zdravotnictví">Zdravotnictví</a></li>
+                        <li><a href=""><img src="img-system/title/services/police officer.3.3.1.png" alt="Městská policie">Městská policie</a></li>
+                        <li><a href=""><img src="img-system/title/services/heart care.3.3.1.png" alt="Sociální péče">Sociální péče</a></li>
+                        <li><a href=""><img src="img-system/title/services/fire.3.3.1.png" alt="Hasiči">Hasiči</a></li>
+                    </ul>
+
+                    <ul>
+                        <li><a href=""><img src="img-system/title/services/bin 1.3.3.1.png" alt="Odpady">Odpady</a></li>
+                        <li><a href=""><img src="img-system/title/services/water drop.3.3.1.png" alt="Voda a kanalizace">Voda a kanalizace</a></li>
+                        <li><a href=""><img src="img-system/title/services/lawn mower.3.3.1.png" alt="Sekání trávy">Sekání trávy</a></li>
+                        <li><a href=""><img src="img-system/title/services/search.3.3.1.png" alt="Ztráty a nálezy">Ztráty a nálezy</a></li>
+                    </ul>
+                </nav>
+
+                <div id='service-nav-bottom'>
+
+                    <h3>Potřebuji si zařídit - TOP 20</h3>
+                    <nav>
+                        <ul>
+                            <li><a href="">Doklady lorem ispum</a></li>
+                            <li><a href="">Poplatky</a></li>
+                            <li><a href="">Vyhlášky lorem ispum</a></li>
+                            <li><a href="">Parkovací karta</a></li>
+                            <li><a href="">Lorem ipsum</a></li>
+                        </ul>
+
+                        <ul>
+                            <li><a href="">Doklady lorem ispum</a></li>
+                            <li><a href="">Poplatky</a></li>
+                            <li><a href="">Vyhlášky lorem ispum</a></li>
+                            <li><a href="">Parkovací karta</a></li>
+                            <li><a href="">Lorem ipsum</a></li>
+                        </ul>
+
+                        <ul>
+                            <li><a href="">Doklady lorem ispum</a></li>
+                            <li><a href="">Poplatky</a></li>
+                            <li><a href="">Vyhlášky lorem ispum</a></li>
+                            <li><a href="">Parkovací karta</a></li>
+                            <li><a href="">Lorem ipsum</a></li>
+                        </ul>
+
+                        <ul>
+                            <li><a href="">Doklady lorem ispum</a></li>
+                            <li><a href="">Poplatky</a></li>
+                            <li><a href="">Vyhlášky lorem ispum</a></li>
+                            <li><a href="">Parkovací karta</a></li>
+                            <li><a href="">Lorem ipsum</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="wrap-side">
+                <a class="but but-emergency" href="">
+                    <img src="img-system/title/event-discussion.png" alt="přednáška">
+                    <strong>Pohotovosti </strong>
+                    <span>Rozpis služeb</span>
+                </a>
+
+                <nav id='service-nav-right'>
+                    <ul>
+                        <li><a href="">Strategický a akční plán</a></li>
+                        <li><a href="">Územní plánování</a></li>
+                        <li><a href="">Developerské projekty</a></li>
+                        <li><a href="">Výběrová řízení</a></li>
+                        <li><a href="">Profil zadavatele</a></li>
+                        <li><a href="">Prostory k pronajmutí</a></li>
+                        <li><a href="">Objekty na prodej</a></li>
+                        <li><a href="">Pozemky na prodej</a></li>
+                        <li><a href="">Městské služby Litomyšl s.r.o.</a></li>
+                        <li><a href="">Městské lesy</a></li>
+                        <li><a href="">Katalog firem a podnikatelů</a></li>
+                        <li><a href="">Živnostenský úřad</a></li>
+                    </ul>
+                </nav>
+
+                <nav id='service-tourists'>
+                    <ul>
+                        <li><a href=""><img src="img-system/title/services/bed single.3.3.1.png" alt="Ubytování">Ubytování</a></li>
+                        <li><a href=""><img src="img-system/title/services/fork knife.3.3.1.png" alt="Stravování">Stravování</a></li>
+                    </ul>
+                </nav>
+
+            </div>
+            <a class="next-section-link" href="#mestsky-urad">Městský úřad</a>
+        </section>
+
+
+        <section id="">
+            <div class="wrap-main">
+                <h2></h2>
+            </div>
+            <div class="wrap-side">
+
+            </div>
+            <a class="next-section-link" href="#"></a>
+        </section>
+
+
+
+        <section id="">
+            <div class="wrap-main">
+                <h2></h2>
+            </div>
+            <div class="wrap-side">
+
+            </div>
+            <a class="next-section-link" href="#"></a>
+        </section>
+
+
+
+        <section id="">
+            <div class="wrap-main">
+                <h2></h2>
+            </div>
+            <div class="wrap-side">
+
+            </div>
+            <a class="next-section-link" href="#"></a>
+        </section>
 
         <?php  include("layouts/_meta.php");  ?>
     </body>
---
+++ layouts/_facebookSide.php
@@ -0,0 +1,9 @@
+<!-- Realcombiz Facebook Widget Start -->
+<script type="text/javascript">/*<![CDATA[*/jQuery(document).ready(function() {jQuery(".rblikebox").hover(function() {jQuery(this).stop().animate({right: "0"},
+        "medium");}, function() {jQuery(this).stop().animate({right: "-250"}, "medium");}, 500);});/*]]>*/
+</script>
+<div class="rblikebox">
+    <div>
+        <iframe src="http://www.facebook.com/plugins/likebox.php?href=http://www.facebook.com/Litomysl.oficialni&amp;width=245&amp;colorscheme=light&amp;show_faces=true&amp;connections=9&amp;stream=true&amp;header=false&amp;height=500" scrolling="no" frameborder="0" scrolling="no" style="border: medium none; overflow: hidden; height: 500px; width: 245px;background:#fff;"></iframe>
+    </div>
+</div>
