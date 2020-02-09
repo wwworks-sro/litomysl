fec11ee9a6319d0798351263d7d4e9fc847048ab
MatyasHajek <hajek@wwworks.cz>
2020-02-09 22:45:43 +0100

    CSS

--- kalendar.php
+++ kalendar.php
@@ -8,25 +8,28 @@
 
 <!--  -----------------------------------Kalendář-----------------------------------------------  -->
 
-        <section id="kalendar" class="red-layout">
+        <section id="kalendar" class="gray-layout">
             <div class="wrap-main">
                 <div class="pad-main-left">
                     <div class="sub-header">
-                        <img class="subpage-icon" src="img/img-system/calendar/calendar-icon.png" alt="kalendář">
+                        <img class="subpage-icon" src="img/img-system/calendar/calendar-detail.png" alt="kalendář">
                         <div class="header-text">
                             <div class="breadcrumb">
-                                <a href="">Volný čas</a> / a href="">turista</a>   >   Kalendář akcí
-                                <h1>
-                                    <span>30. září 2019 </span>
-                                    /  pátek
+                                <a href="">Volný čas / turista</a>   >   Kalendář akcí
+                            </div>
+                                <h1 class="subpage-title">
+                                    30. září 2019
+                                    <span>/  pátek </span>
                                     <strong>Kalendář akcí</strong>
                                 </h1>
-                            </div>
                         </div>
+                        <div class="cleaner"></div>
                     </div>
                     <div class="cal-links-main">
                         <div class="cal-item-detail">
-                            <img src="img/img-system/tourist/event-red-drama.png" alt="divadlo">
+                            <div class="cal-detail-text">
+                                <img class="cal-detail-img" src="img/img-system/tourist/event-red-drama.png" alt="divadlo">
+                            </div>
                             <div class="cal-detail-text">
                                 <a href=""><h4>Lorem ipsum dolor smet, lo ipsum dolor</h4></a>
                                 <div class="cal-date-location"><strong>30. září</strong>, 8:00   /  <a href=""><img src="img/img-system/calendar/pointer.png" alt="Místo"> Smetanovo náměstí</a></div>
@@ -52,43 +55,15 @@
 
                     </div>
 
-                    <div class="cal-links-continuous">
-                        <div class="cal-item-detail">
-                            <img src="img/img-system/tourist/event-red-drama.png" alt="divadlo">
-                            <div class="cal-detail-text">
-                                <a href=""><h4>Lorem ipsum dolor smet, lo ipsum dolor</h4></a>
-                                <div class="cal-date-location"><strong>30. září</strong>, 8:00   /  <a href=""><img src="img/img-system/calendar/pointer.png" alt="Místo"> Smetanovo náměstí</a></div>
-                                <div class="cal-perex">
-                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam libero. Nam libero tempore. Ipsum dolor si Dolor sit amet, elit. Nam libero tempore. Lorem ipsum dolor sit amet, consectetuer ero tempore. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam libero. Sit amet, elit. Nam libero tempore.
-                                        Více...</p>
-                                </div>
-                                <div class="cal-perex">
-                                    <a href="">
-                                        Přidat do Google kalendáře
-                                    </a>
-                                    <a class="cal-google-link" href="">Přidat do Google kalendáře</a>
-                                    <a class="cal-web=link" href="">web akce</a>
-                                    <a class="cal-organisation-link" href="">Smetanův dům</a>
-                                </div>
-                            </div>
-                            <div class="cal-detail-image">
-                                <img src="img/img-system/tourist/event-red-drama.png" alt="divadlo">
-                                share,
-                            </div>
-                            <a class="cal-detail-link cal-detail-link" href=""></a>
-                        </div>
-
-                    </div>
                     <div class="cleaner"></div>
                 </div>
             </div>
-            <div class="wrap-side bg-red">
+            <div class="wrap-side">
                 <div class="pad-main-right">
 
                 </div>
                 <div class="cleaner"></div>
             </div>
-            <a class="next-section-link" href="#o-litomysli">O&nbsp;Litomyšli</a>
             <div class="cleaner"></div>
         </section>
 
--- layouts/_head.php
+++ layouts/_head.php
@@ -21,6 +21,11 @@
         <link rel="stylesheet" type="text/css" href="css/tourist.css" />
     <?php } ?>
 
+<!--  Podstránky mimo titulku a turistu  -->
+    <?php  if($fileName != 'turista.php' && $fileName != 'index.php' ){  ?>
+        <link rel="stylesheet" type="text/css" href="css/subpages.css" />
+    <?php } ?>
+
     <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
 </head>
