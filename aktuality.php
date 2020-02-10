96926d0a8284a01a37892e233e610294c94202a1
MatyasHajek <hajek@wwworks.cz>
2020-02-10 23:27:47 +0100

    CSS

---
+++ aktuality.php
@@ -0,0 +1,117 @@
+
+<!DOCTYPE html>
+
+    <?php  include("layouts/_head.php");  ?>
+
+    <body class="sub-page">
+        <?php  include('layouts/_header.php');  ?>
+
+<!--  -----------------------------------Kalendář-----------------------------------------------  -->
+
+        <section id="kalendar" class="red-layout">
+            <div class="wrap-main">
+                <div class="pad-main-left">
+                    <div class="sub-header">
+                        <img class="subpage-icon" src="img/img-system/subpages/newspaper.3.3.1.png" alt="kalendář">
+                        <div class="header-text">
+                            <div class="breadcrumb">
+                                <a href="">Občan / radnice</a>   >   Aktuality
+                            </div>
+                                <h1 class="subpage-title">
+                                    Název aktuality lorem ipsum
+                                    <strong>Aktuality</strong>
+                                </h1>
+                        </div>
+                        <div class="cleaner"></div>
+                    </div>
+                    <div class="news-detail-images">
+                        <img class="img-news-main" src="img/img-articles/L/news-lorem-ipsum-l.jpg" alt="Lorem ipsum dolor">
+                        <div class="news-detail-gallery">
+
+                        </div>
+                    </div>
+                    <div class="perex">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam libero. Nam libero tempore. Ipsum dolor si Dolor sit amet, elit. Nam libero tempore. Lorem ipsum dolor sit amet, consectetuer ero tempore. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Fusce suscipit libero eget elit. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Integer in sapien. Aliquam in lorem sit amet leo accumsan lacinia. Integer in sapien. Nulla non arcu lacinia neque faucibus fringilla. Nulla quis diam. Aenean vel massa quis mauris vehicula lacinia. Vivamus luctus egestas leo. Phasellus et lorem id felis nonummy placerat.</p>
+                    <p>Praesent in mauris eu tortor porttitor accumsan. Mauris tincidunt sem sed arcu. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Fusce consectetuer risus a nunc. Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo. Curabitur sagittis hendrerit ante. Curabitur vitae diam non enim vestibulum interdum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Praesent vitae arcu tempor neque lacinia pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus in lacus. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Nulla quis diam. Aliquam ornare wisi eu metus. Aenean vel massa quis mauris vehicula lacinia.</p>
+                    <p>Curabitur sagittis hendrerit ante. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Sed convallis magna eu sem. Aliquam erat volutpat. Fusce wisi. Nulla non lectus sed nisl molestie malesuada. Aliquam id dolor. Aliquam erat volutpat. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Nullam dapibus fermentum ipsum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem.</p>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Etiam posuere lacus quis dolor. Aenean placerat. Aliquam erat volutpat. Pellentesque ipsum. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Integer in sapien. Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Suspendisse nisl. Mauris tincidunt sem sed arcu. Etiam neque. Vivamus luctus egestas leo. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+
+
+                    <div class="news-detail-footer">
+                        <p><strong>Mgr. Martin Novák</strong><br />
+                        vedoucí lorem ipsum dolor sit amet </p>
+
+                        <p>datum zveřejnění: 28. 9. 2019<br />
+                        datum aktualizace: 29. 9. 2019<br />
+                        zveřejnil: Mgr. Martin Novák</p>
+                    </div>
+                    <div class="cleaner"></div>
+                </div>
+            </div>
+            <div class="wrap-side  bg-red">
+                <div class="pad-main-right">
+                    <div class="sub-header-side">
+                        <div id="fb-root"></div>
+                        <script>(function(d, s, id) {
+                                var js, fjs = d.getElementsByTagName(s)[0];
+                                if (d.getElementById(id)) return;
+                                js = d.createElement(s); js.id = id;
+                                js.src = 'https://connect.facebook.net/cs_CZ/sdk.js#xfbml=1&version=v3.0';
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }(document, 'script', 'facebook-jssdk'));</script>
+                        <div class="fb-like" style="margin-top:5px; margin-bottom:30px;" data-href="url-stranky-akce" data-layout="button" data-action="recommend" data-size="large" data-show-faces="true" data-share="true"></div>
+                    </div>
+
+                    <div class="news-other">
+                        <a href="" class="news-title news-mini">
+                            <img src="img/img-articles/S/news-amet-dolor-s.jpg" alt="Lorem ipsum dolor">
+                            <div class="news-text">
+                                <h6>Aktualita 1 dolor sit amet tetuer adip</h6>
+                                <p>Lorem ipsum dolor sit amet, tetuer adipiscing elit. Nam libero </p>
+                            </div>
+                            <div class="cleaner"></div>
+                        </a>
+                        <a href="" class="news-title news-mini">
+                            <img src="img/img-articles/S/news-consectetur-s.jpg" alt="Lorem ipsum dolor">
+                            <div class="news-text">
+                                <h6>Aktualita 1 dolor sit amet tetuer adip</h6>
+                                <p>Lorem ipsum dolor sit amet, tetuer adipiscing elit. Nam libero </p>
+                            </div>
+                            <div class="cleaner"></div>
+                        </a>
+                        <a class="news-title news-mini">
+                            <img src="img/img-articles/S/news-dolor-sit-2-s.jpg" alt="Lorem ipsum dolor">
+                            <div class="news-text">
+                                <h6>Aktualita 1 dolor sit amet tetuer adip</h6>
+                                <p>Lorem ipsum dolor sit amet, tetuer adipiscing elit. Nam libero </p>
+                            </div>
+                            <div class="cleaner"></div>
+                        </a>
+                        <a href="" class="news-title news-mini">
+                            <img src="img/img-articles/S/news-lore-ipsum-s.jpg" alt="Lorem ipsum dolor">
+                            <div class="news-text">
+                                <h6>Aktualita 1 dolor sit amet tetuer adip</h6>
+                                <p>Lorem ipsum dolor sit amet, tetuer adipiscing elit. Nam libero </p>
+                            </div>
+                            <div class="cleaner"></div>
+                        </a>
+                        <a href="" id="more-news-mini" href="https://www.litomysl.cz/aktuality">
+                            <div class="more-news-mini">
+                                <img src="img/img-system/culture/dots.png" alt="Lorem ipsum dolor">
+                            </div>
+                            <div class="news-text">
+                                Další aktuality
+                            </div>
+                        </a>
+                    </div>
+                </div><div class="cleaner"></div>
+                </div>
+            <div class="cleaner"></div>
+        </section>
+
+        <?php  include("layouts/_footer.php");  ?>
+        <?php  include('layouts/_facebookSide.php');  ?>
+    </body>
+</html>
